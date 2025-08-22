8ec65343732bdd4ae314db4efa485b7f1218502d
Seika <artistnoob6@gmail.com>
2025-08-22 11:07:26 +0530

    fixing

--- models/User.js
+++ models/User.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 
-const UserSchema=new mongoose.Schema(
-	{
-		_id:{type:String,required:true},
-		name:{type:String,required:true},
-		email:{type:String,require:true},
-		image:{type:String,require:false}
-
-
-
-	},{timestamps:true}
-
+const UserSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: false }
+  },
+  { timestamps: true }
 );
 
-const User =mongoose.model.User || mongoose.model("User", UserSchema)
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
+
+
